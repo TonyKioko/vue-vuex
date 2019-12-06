fedb2068ed1778b07764eaf44ac4f190eacb773a
TonyKioko <tonykioko384@gmail.com>
2019-12-06 20:07:51 +0300

    validating props

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="part" :class="position">
-    <img :src="selectedPart.src" title="arm"/>
+    <img :src="selectedPart.src" title="arm" />
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
     <span class="sale" v-show="selectedPart.onSale">Sale!</span>
@@ -8,7 +8,6 @@
 </template>
 
 <script>
-
 function getPreviousValidIndex(index, length) {
   const deprecatedIndex = index - 1;
   return deprecatedIndex < 0 ? length - 1 : deprecatedIndex;
@@ -20,39 +19,48 @@ function getNextValidIndex(index, length) {
 }
 
 export default {
-  props:['parts','position'],
+  props: { parts: {
+    type:Array,
+    required:true
+  }, position: {
+    type:String,
+    required:true,
+    validator:function(value){
+        return ['top','left','center','right','bottom'].includes(value)
+    }
+  } 
+  
+  },
   data() {
     return { selectedPartIndex: 0 };
   },
   computed: {
     selectedPart() {
       return this.parts[this.selectedPartIndex];
-    },
+    }
   },
   methods: {
     selectNextPart() {
       this.selectedPartIndex = getNextValidIndex(
         this.selectedPartIndex,
-        this.parts.length,
+        this.parts.length
       );
     },
     selectPreviousPart() {
       this.selectedPartIndex = getPreviousValidIndex(
         this.selectedPartIndex,
-        this.parts.length,
+        this.parts.length
       );
-    },
-
-  },
+    }
+  }
 };
-
 </script>
 
 <style scoped>
 .part {
   position: relative;
-  width:165px;
-  height:165px;
+  width: 165px;
+  height: 165px;
   border: 3px solid #aaa;
 }
 .sale {
@@ -73,7 +81,7 @@ export default {
   top: -25px;
 }
 .part img {
-  width:165px;
+  width: 165px;
 }
 .top {
   border-bottom: none;
@@ -95,7 +103,7 @@ export default {
 }
 .prev-selector {
   position: absolute;
-  z-index:1;
+  z-index: 1;
   top: -3px;
   left: -28px;
   width: 25px;
@@ -103,26 +111,33 @@ export default {
 }
 .next-selector {
   position: absolute;
-  z-index:1;
+  z-index: 1;
   top: -3px;
   right: -28px;
   width: 25px;
   height: 171px;
 }
-.left .prev-selector:after,  .right .prev-selector:after{
-  content: '\25B2'
+.left .prev-selector:after,
+.right .prev-selector:after {
+  content: "\25B2";
 }
-.left .next-selector:after, .right .next-selector:after {
-  content: '\25BC'
+.left .next-selector:after,
+.right .next-selector:after {
+  content: "\25BC";
 }
-.top .prev-selector:after, .bottom .prev-selector:after, .center .prev-selector:after{
-  content: '\25C4'
+.top .prev-selector:after,
+.bottom .prev-selector:after,
+.center .prev-selector:after {
+  content: "\25C4";
 }
-.top .next-selector:after, .bottom .next-selector:after, .center .next-selector:after{
-  content: '\25BA'
+.top .next-selector:after,
+.bottom .next-selector:after,
+.center .next-selector:after {
+  content: "\25BA";
 }
-.center .prev-selector, .center .next-selector {
-  opacity:0.8;
+.center .prev-selector,
+.center .next-selector {
+  opacity: 0.8;
 }
 .left .prev-selector {
   top: -28px;
